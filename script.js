a8b42fbab174211da216e9cdb5f549da56effd50
dev-davimarques <davimarques10.dm@icloud.com>
2025-02-02 15:43:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Coletar valores dos coeficientes
+const a = parseFloat(document.getElementById('a').value);
+const b = parseFloat(document.getElementById('b').value);
+const c = parseFloat(document.getElementById('c').value);
---
+++ teste.js
@@ -0,0 +1,1 @@
+console.log('Teste auto restart')
