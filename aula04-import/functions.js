ec4c3b30408eae93198b7b89dabe717ee4759c76
dev-davimarques <davimarques10.dm@icloud.com>
2025-02-21 19:31:20 -0300

    utilizando o import/export em js

---
+++ aula04-import/functions.js
@@ -0,0 +1,11 @@
+function areaRetangulo(base, altura){
+    return base * altura
+}
+
+function areaTriangulo(base, altura){
+    return (base * altura) / 2
+}
+
+// Fazer função com arrow function
+
+export {areaRetangulo, areaTriangulo}
---
+++ aula04-import/script.js
@@ -0,0 +1,4 @@
+import {areaRetangulo, areaTriangulo } from './functions.js'
+
+console.log("Área do ratângulo: ",areaRetangulo(2,4))
+console.log("Área do triângulo: ",areaTriangulo(3,5))
