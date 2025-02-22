928feea9f30383606c1f9f088d9bcd6106c72dfc
dev-davimarques <davimarques10.dm@icloud.com>
2025-02-21 21:37:24 -0300

    utilizando um app react

---
+++ aula04-reactapp/react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Title from "./components/title/title"
+
+function App() {
+  return(
+    <>
+      <h1>Lista de Compras</h1>
+      <Title />
+
+      <ul>
+        <li>Laranja</li>
+        <li>Mamão</li>
+        <li>Maçã</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ aula04-reactapp/react-app/src/components/title/title.jsx
@@ -0,0 +1,11 @@
+function Title(){
+    return(
+        <div>
+            <label>Produtos</label>
+            <br />
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Title
